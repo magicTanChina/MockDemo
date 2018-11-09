3c1405a06edef6880cd2e329165471c6e29efc04
TAN <854918599@qq.com>
2018-11-10 01:18:13 +0800

    测试提交账号

--- src/test/java/com/magictan/mockdemo/localservice/impl/LocalServiceImplTest.java
+++ src/test/java/com/magictan/mockdemo/localservice/impl/LocalServiceImplTest.java
@@ -32,7 +32,7 @@ public class LocalServiceImplTest {
      */
     @Test
     public void test() {
-        //test commit2
+        //test commit3
         Node target = new Node(1, "target");
         Mockito.when(remoteService.getRemoteNode(1)).thenReturn(target);
         Node result = localService.getRemoteNode(1);
